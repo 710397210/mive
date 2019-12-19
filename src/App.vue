8d4de166f6947123cb41cd5429c530328953c063
710397210 <710397210@qq.com>
2019-12-19 10:48:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">
+        <el-menu mode="horizontal"
+                 background-color="#545c64"
+                 text-color="#fff"
+                 active-text-color="#ffd04b"
+                 default-active="/"
+                 :router="true">
+          <el-menu-item index="/">选电影</el-menu-item>
+          <el-menu-item index="/top">排行榜</el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="center" style="background-color: #aa4c1c">
+      <el-col :span="4">
+        <h2 style="color:#27a">豆瓣电影</h2>
+      </el-col>
+      <el-col :span="8" style="line-height: 71px">
+        <el-input placeholder="搜索电影、电视剧、综艺、艺人">
+          <template slot="append">
+            <i class="el-icon-search"></i>
+          </template>
+        </el-input>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <router-view/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/demo.vue
@@ -0,0 +1,30 @@
+<template>
+    <!--template-->
+    <div>
+        <h1>{{content}}</h1>
+        请输入内容：<input type="text" v-model="content">
+        <el-button type="warning" round="">按钮</el-button>
+    </div>
+</template>
+<!--template代表的就是html的内容，相当于<div id="app">标签中的内容-->
+<script>
+    import ElButton from "../../node_modules/element-ui/packages/button/src/button";
+    /*<script>*/
+
+
+    export default {
+        //在.vue文件中定义
+        components: {ElButton},
+        data:function () {
+            //return返回数据，将对象通过return返回给vue-cli
+            return {
+                content:""
+        }
+        }
+    }
+</script>
+<style scoped>/*scoped含义：将当前vue文件中css属性的作用域限制在当前的页面，不会对其他页面产生影响*/
+    h1{
+        color: red
+    }
+</style>
